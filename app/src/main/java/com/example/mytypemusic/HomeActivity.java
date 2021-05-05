8dd36e4e83f65e6c1e3a33d3a324eaad201b3d6c
Manish8798 <mankum08071998@gmail.com>
2021-05-05 16:02:56 +0530

    firebase storage and database added

--- app/src/main/java/com/example/mytypemusic/HomeActivity.java
+++ app/src/main/java/com/example/mytypemusic/HomeActivity.java
@@ -1,26 +1,57 @@
 package com.example.mytypemusic;
 
+import android.Manifest;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.OpenableColumns;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.example.mytypemusic.databinding.ActivityHomeBinding;
+import com.example.mytypemusic.fragments.FavoritesFragment;
+import com.example.mytypemusic.fragments.HomeFragment;
+import com.example.mytypemusic.model.SongDetails;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final int RC_UPLOAD = 1;
     private ActivityHomeBinding binding;
+    private boolean checkPermission = false;
+    private Uri uri;
+    private String songName, songUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +62,94 @@ public class HomeActivity extends AppCompatActivity {
         binding.bottomNav.setBackground(null);
         binding.bottomNav.getMenu().getItem(1).setEnabled(false);
 
+        binding.fab.setOnClickListener(v -> {
+            if (validatePermission()) {
+                uploadSong();
+            }
+        });
+        binding.bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.home_container, new HomeFragment()).commit();
+
         GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
         if (signInAccount != null) {
             Toast.makeText(this, "Welcome " + signInAccount.getDisplayName(), Toast.LENGTH_SHORT).show();
         }
     }
+
+    private void uploadSong() {
+        Intent intentUpload = new Intent();
+        intentUpload.setType("audio/*");
+        intentUpload.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intentUpload, RC_UPLOAD);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == RC_UPLOAD && resultCode == RESULT_OK) {
+            uri = data.getData();
+            Cursor cursor = getApplicationContext().getContentResolver().query(uri, null, null, null, null);
+            int index = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+            cursor.moveToFirst();
+            songName = cursor.getString(index);
+            cursor.close();
+            uploadToFirebaseStorage();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void uploadToFirebaseStorage() {
+
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference()
+                .child("Songs")
+                .child(uri.getLastPathSegment());
+
+        binding.progressSeekBar.setVisibility(View.VISIBLE);
+
+        storageReference.putFile(uri).addOnSuccessListener(taskSnapshot -> {
+
+            Handler handler = new Handler();
+            handler.postDelayed(() -> binding.progressSeekBar.setProgress(0), 1000);
+            Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+            while (!uriTask.isComplete()) ;
+            Uri urlSong = uriTask.getResult();
+            songUrl = urlSong.toString();
+            uploadDetailsToFirebase();
+
+        }).addOnFailureListener(e -> {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            binding.progressSeekBar.setVisibility(View.GONE);
+
+        }).addOnProgressListener(snapshot -> {
+            double progress = (100.0 * snapshot.getBytesTransferred()) / snapshot.getTotalByteCount();
+            int currentProgress = (int) progress;
+            binding.progressSeekBar.post(() -> binding.progressSeekBar.setProgress(currentProgress));
+        });
+
+    }
+
+    private void uploadDetailsToFirebase() {
+
+        SongDetails songDetails = new SongDetails(songName, songUrl);
+        FirebaseDatabase.getInstance().getReference("Songs").push().setValue(songDetails)
+                .addOnCompleteListener(task -> Toast.makeText(HomeActivity.this, "Song Uploaded", Toast.LENGTH_SHORT).show()).addOnFailureListener(e -> Toast.makeText(HomeActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show());
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
+        Fragment selectedFragment = null;
+        switch (item.getItemId()) {
+            case R.id.home:
+                selectedFragment = new HomeFragment();
+                break;
+
+            case R.id.favorite:
+                selectedFragment = new FavoritesFragment();
+                break;
+        }
+        assert selectedFragment != null;
+        getSupportFragmentManager().beginTransaction().replace(R.id.home_container, selectedFragment).commit();
+        return true;
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,5 +174,28 @@ public class HomeActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private boolean validatePermission() {
+        Dexter.withContext(HomeActivity.this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        checkPermission = true;
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        checkPermission = false;
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+
+        return checkPermission;
     }
 }
---
+++ app/src/main/java/com/example/mytypemusic/model/SongDetails.java
@@ -0,0 +1,31 @@
+package com.example.mytypemusic.model;
+
+public class SongDetails {
+
+    private String songName;
+    private String songUrl;
+
+    public SongDetails() {
+    }
+
+    public SongDetails(String songName, String songUrl) {
+        this.songName = songName;
+        this.songUrl = songUrl;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getSongUrl() {
+        return songUrl;
+    }
+
+    public void setSongUrl(String songUrl) {
+        this.songUrl = songUrl;
+    }
+}
