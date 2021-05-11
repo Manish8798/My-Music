9e57ac8df91f8b9a004d759656392ab0324cc5f0
Manish <mankum08071998@gmail.com>
2021-05-11 15:36:56 +0530

    song time added and ui improved

--- app/src/main/java/com/example/mytypemusic/PlayerActivity.java
+++ app/src/main/java/com/example/mytypemusic/PlayerActivity.java
@@ -6,6 +6,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,6 +23,8 @@ public class PlayerActivity extends AppCompatActivity {
     private ActivityPlayerBinding binding;
     private MediaPlayer mediaPlayer;
     private boolean playingAudio = false;
+    Runnable runnable;
+    Handler handler;
 
     @SuppressLint("UseCompatLoadingForDrawables")
     @Override
@@ -42,7 +45,7 @@ public class PlayerActivity extends AppCompatActivity {
         }
         binding.songName.setText(songName);
         Log.d(TAG, "onCreate: " + songUrl);
-        Handler handler = new Handler();
+        handler = new Handler();
 
         mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioAttributes(
@@ -61,8 +64,17 @@ public class PlayerActivity extends AppCompatActivity {
 
         mediaPlayer.setOnPreparedListener(mp -> {
             binding.playBtn.setBackground(getDrawable(R.drawable.ic_pause));
+            binding.seekbarPlayer.setMax(mp.getDuration());
+            binding.seekBarEndTime.setText(milliSecToTimer(mp.getDuration()));
             mediaPlayer.start();
             playingAudio = true;
+            updateSeekbar();
+        });
+
+        mediaPlayer.setOnBufferingUpdateListener((mp, percent) -> {
+            double ratio = percent / 100.0;
+            int bufferingLevel = (int) (mp.getDuration() * ratio);
+            binding.seekbarPlayer.setSecondaryProgress(bufferingLevel);
         });
 
         binding.playBtn.setOnClickListener(v -> {
@@ -84,11 +96,72 @@ public class PlayerActivity extends AppCompatActivity {
 
         });
 
+        binding.seekbarPlayer.setOnSeekBarChangeListener(seekBarChangeListener);
+
+    }
+
+    private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            if (fromUser) {
+                mediaPlayer.seekTo(progress);
+                seekBar.setProgress(progress);
+            }
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+    private void updateSeekbar() {
+        try {
+            if (mediaPlayer != null) {
+                long currentTime = mediaPlayer.getCurrentPosition();
+                int currentPos = (int) currentTime;
+                binding.seekBarStartTime.setText(milliSecToTimer(currentTime));
+                binding.seekbarPlayer.setProgress(currentPos);
+                runnable = this::updateSeekbar;
+                handler.postDelayed(runnable, 500);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d(TAG, "updateSeekbar: " + e.getMessage());
+        }
+
+    }
+
+    private String milliSecToTimer(long milliseconds) {
+        String timerString = "";
+        String secondString;
+
+        int hours = (int) (milliseconds / (1000 * 60 * 60));
+        int minutes = (int) (milliseconds % (1000 * 60 * 60)) / (1000 * 60);
+        int seconds = (int) ((milliseconds % (1000 * 60 * 60)) % (1000 * 60) / 1000);
+
+        if (hours > 0) {
+            timerString = hours + ":";
+        }
+        if (seconds < 10) {
+            secondString = "0" + seconds;
+        } else {
+            secondString = "" + seconds;
+        }
+
+        timerString = timerString + minutes + ":" + secondString;
+        return timerString;
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
+        clearMediaPlayer();
         return true;
     }
 
